9d4c9685b24554f21fa9b723bb8ef0de7a90a1b6
Nick Tassinari <ntass7800@gmail.com>
2023-03-29 09:40:13 -0600

    add nickname and set nickname

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,10 @@
 class Player
-  attr_reader :name, :monthly_cost, :contract_length
+  attr_reader :name, :monthly_cost, :contract_length, :nickname
   def initialize(name, monthly_cost, contract_length)
     @name = name 
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+    @nickname = nil 
   end
 
   def first_name 
@@ -12,5 +13,13 @@ class Player
 
   def last_name 
     @name.split[1]
+  end
+
+  def total_cost
+    monthly_cost * contract_length
+  end
+
+  def set_nickname!(nickname)
+    @nickname = nickname 
   end
 end
